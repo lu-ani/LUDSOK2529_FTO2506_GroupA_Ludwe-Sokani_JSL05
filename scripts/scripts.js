e7c973490fa3e4e38e2d44d3e77d75d647ee46ee
Ludwe Sokani <ludwesokani@gmail.com>
2025-09-21 15:46:37 +0200

    Check local storage for data

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,23 @@
 import { initialTask } from "../initialData.js";
 
+var initialHolder = localStorage.getItem("initialTask");
+
+if (initialHolder !== null) {
+  var initialTasks = JSON.parse(initialHolder);
+} else {
+  var initialTasks = initialTask;
+}
  * Initializes the task board and modal handlers.
  */
 export function initTaskBoard() {
   clearExistingTasks();
+  if (initialHolder !== null) {
+    var initialTasks = JSON.parse(initialHolder);
+    console.log("using saved initial data");
+    renderTasks(initialTasks);
+  } else {
+    var initialTasks = initialTask;
+    console.log("using initial data tasks");
     renderTasks(initialTasks);
   }
   setupModalCloseHandler();
